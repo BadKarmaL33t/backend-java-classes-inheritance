61c7a7d64505a5fc212efe6920810f64fc3e156d
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-06-06 12:54:39 +0200

    opdracht classes inheritance afmaken

--- src/main/java/CreditCard.java
+++ src/main/java/CreditCard.java
@@ -1,4 +1,4 @@
-class CreditCard {
+abstract class CreditCard {
 
     private int debt;
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        Customer customer = new Customer("Frodo", "Balings", new CreditCard(100));
+        Customer customer = new Customer("Frodo", "Balings", new VisaCard(0));
 
-
-        customer.getCreditCard().pay(10);
+        customer.getCreditCard().pay(100);
         customer.printName();
         System.out.println(customer.getCreditCard().getDebt());
+
+        Customer customer2 = new Customer("Jos", "Hagesteijn", new MasterCard(0));
+
+        customer2.getCreditCard().pay(100);
+        customer2.printName();
+        System.out.println(customer2.getCreditCard().getDebt());
     }
 }
